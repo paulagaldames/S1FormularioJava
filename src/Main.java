a14580552a3a7c1927af5236f7da64e6ab762678
Paula Galdames <sitio.galdames@gmail.com>
2025-06-07 20:42:32 -0400

    Ingreso de todos los componentes al Main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,50 @@
-
+//importamos las librerias correspondientes
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        JFrame frame = new JFrame("Registro de Eventos - Universidad Albert Einstein");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLayout(new GridLayout(9, 2, 10, 10));
+
+        //etiquetas label
+        JLabel nombreLabel = new JLabel("Nombre:");
+        JLabel correoLabel = new JLabel("Correo:");
+        JLabel telefonoLabel = new JLabel("Telefono:");
+
+        //etiquetas de entrada textos
+        JTextField nombreText = new JTextField();
+        JTextField correoText = new JTextField();
+        JTextField telefonoText = new JTextField();
+
+        //listas desplegables
+        String[] eventos = {"Charla de IA", "Taller de Ciberseguridad", "Presentación de Proyectos", "Networking Profesional"};
+        JComboBox<String> eventoComboBox = new JComboBox<>(eventos);
+
+        String[] comida = {"Ninguna preferencia", "Vegetariano", "Vegano", "Sin gluten"};
+        JComboBox<String> comidaComboBox = new JComboBox<>(comida);
+
+        // CheckBoxes
+        JCheckBox accesibilidadCheckBox = new JCheckBox("Accesibilidad para discapacitados");
+        JCheckBox traduccionCheckBox = new JCheckBox("Necesito traducción");
+
+        //Radiobutton
+        JRadioButton masculinoRadioButton = new JRadioButton("Masculino");
+        JRadioButton femeninoRadioButton = new JRadioButton("Femenino");
+        ButtonGroup generoGroup = new ButtonGroup();
+        generoGroup.add(femeninoRadioButton);
+        generoGroup.add(masculinoRadioButton);
+
+        //boton de envio
+        JButton submitButton = new JButton("Enviar");
+
+
+
     }
 }
